589922d98048c932dea2ba1152749b6a8da26a36
Shiv Chawla <shivchawla2001@gmail.com>
2016-12-23 19:55:18 +0530

    a

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var vulcanize = require('gulp-vulcanize');
+var crisper = require('gulp-crisper');
+var htmlmin = require('gulp-html-minifier');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+ 
+gulp.task('vulcanize', function () {
+    return gulp.src('src/aq-dashboard/aq-dashboard.html')
+        .pipe(vulcanize({
+            abspath: '',
+            //excludes: ['bower_components/nvd3-elements/','bower_components/juicy-ace-editor/','bower_components/simplemde/','bower_components/webcomponentsjs/','bower_components/polymer/'],
+            excludes: ['bower_components/vaadin-icons/'],
+            stripExcludes: false,
+            //inlineScripts:true,
+            //inlineCss:true,
+            stripComments:true,
+
+        })).pipe(crisper())
+        .pipe(gulp.dest('build/src/aq-dashboard'));
+});
+
+ 
+gulp.task('compress', ['vulcanize'], function (cb) {
+  pump([
+        gulp.src('build/src/aq-dashboard/aq-dashboard.js'),
+        uglify(),
+        gulp.dest('build/src/aq-dashboard')
+    ],
+    cb
+  );
+});
+
+gulp.task('copy',['vulcanize'], function() {
+    gulp.src('resources/**/*')
+      .pipe(gulp.dest('build/resources'));
+
+    gulp.src('bower_components/**/*')
+      .pipe(gulp.dest('build/bower_components'));
+
+    gulp.src('index.html')
+      .pipe(gulp.dest('build'));
+});
+
+gulp.task('default',['copy']);  
