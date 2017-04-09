9a5671b5b1925c3840173408e99ff2dca2ca3f3a
Shiv Chawla <shivchawla2001@gmail.com>
2017-04-09 13:42:33 -0600

    Moved JS scripts to resources folder Modified code to comply to ES5 instead of ES6( gulp is till outdated)

---
+++ resources/scripts/DateTime.js
@@ -0,0 +1,55 @@
+/*
+* @Author: Shiv Chawla
+* @Date:   2017-04-07 14:34:31
+* @Last Modified by:   Shiv Chawla
+* @Last Modified time: 2017-04-07 14:35:08
+*/
+
+(function(window){
+    'use strict';
+    function dateLib(){
+        var _dateLib = {};
+        
+        _dateLib.formatDate = function (date) {
+
+            var today = new Date(date);
+            today.setDate(today.getDate());
+            var dd = today.getDate();
+            var mm = today.getMonth() + 1; //January is 0!
+            var yyyy = today.getFullYear();
+
+            if (dd < 10) {
+                dd = '0' + dd
+            }
+
+            if (mm < 10) {
+                mm = '0' + mm
+            }
+
+            today = dd + '/' + mm + '/' + yyyy;
+            return today;
+        };
+
+        _dateLib.formatDateTime = function (date) {
+            date = new Date(date);
+            var hours = date.getHours();
+            var minutes = date.getMinutes();
+            var ampm = hours >= 12 ? 'PM' : 'AM';
+            hours = hours % 12;
+            hours = hours ? hours : 12; // the hour '0' should be '12'
+            minutes = minutes < 10 ? '0' + minutes : minutes;
+            var strTime = hours + ':' + minutes + ' ' + ampm;
+            var mm = date.getMonth() + 1;
+            mm = mm > 9 ? mm : '0' + mm.toString();
+            var dd = date.getDate();
+            dd = dd > 9 ? dd : '0' + dd.toString();
+            return dd + "/" + mm + "/" + date.getFullYear() + "  " + strTime;
+        };
+
+        return _dateLib;
+    }
+
+    if(typeof(window.date) === 'undefined'){
+        window.date = dateLib();
+    }
+})(window);
