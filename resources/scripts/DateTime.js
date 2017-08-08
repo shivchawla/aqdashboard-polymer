cdc722e66165c629d13ef729420662649ce9a127
Shiv Chawla <shivchawla2001@gmail.com>
2017-08-08 11:32:55 +0530

    More changes for new forward testing (unstable)

--- resources/scripts/DateTime.js
+++ resources/scripts/DateTime.js
@@ -2,7 +2,7 @@
 * @Author: Shiv Chawla
 * @Date:   2017-04-07 14:34:31
 * @Last Modified by:   Shiv Chawla
-* @Last Modified time: 2017-04-07 14:35:08
+* @Last Modified time: 2017-08-03 22:07:16
 */
 
 (function(window){
@@ -27,6 +27,26 @@
             }
 
             today = dd + '/' + mm + '/' + yyyy;
+            return today;
+        };
+
+        _dateLib.formatDateForTable = function (date) {
+
+            var today = new Date(date);
+            today.setDate(today.getDate());
+            var dd = today.getDate();
+            var mm = today.getMonth() + 1; //January is 0!
+            var yyyy = today.getFullYear();
+
+            if (dd < 10) {
+                dd = '0' + dd
+            }
+
+            if (mm < 10) {
+                mm = '0' + mm
+            }
+
+            today = yyyy+' - '+mm+' - '+dd;
             return today;
         };
 
