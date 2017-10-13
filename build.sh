fe9e7e39625922eaf5e16006b76515d2b23ba071
Shiv Chawla <shivchawla2001@gmail.com>
2017-10-13 15:46:41 +0530

    Update build file to copy only contents of modified BC components Modified editor-edit to show both log and settings panel at all times

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # @Author: Shiv Chawla
 # @Date:   2017-10-06 17:42:29
 # @Last Modified by:   Shiv Chawla
-# @Last Modified time: 2017-10-06 17:59:16
+# @Last Modified time: 2017-10-10 22:45:34
 #!/bin/bash
-cp -R bower_components_modified/  bower_components/
+cp -R bower_components_modified/.  bower_components/
 polymer build
